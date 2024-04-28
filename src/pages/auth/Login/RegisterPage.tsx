46569682fb5899fc343ba9d018924493d9b5e983
Zlaam <mrzalaam@gmail.com>
2024-04-28 19:13:55 +0500

    update RegisterPage.tsx

--- src/pages/auth/Login/RegisterPage.tsx
+++ src/pages/auth/Login/RegisterPage.tsx
@@ -26,12 +26,12 @@ function RegisterPage() {
             <div className="grid gap-4">
               <div className="grid grid-cols-2 gap-4">
                 <div className="grid gap-2">
-                  <Label htmlFor="first-name">First name</Label>
-                  <Input id="first-name" placeholder="John" required />
+                  <Label htmlFor="username">Username</Label>
+                  <Input id="username" placeholder="john_doe" required />
                 </div>
                 <div className="grid gap-2">
-                  <Label htmlFor="last-name">Last name</Label>
-                  <Input id="last-name" placeholder="Doe" required />
+                  <Label htmlFor="fullname">Full Name</Label>
+                  <Input id="fullname" placeholder="John Doe" required />
                 </div>
               </div>
               <div className="grid gap-2">
