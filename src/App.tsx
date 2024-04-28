e1a923d4a07023d2f8a6a00e918d008c614f5163
Zlaam <mrzalaam@gmail.com>
2024-04-28 18:21:24 +0500

    update 3 files and create 1 file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1>Elib DashBoard</h1>
+      <Button variant={"outline"} className="">
+        Hello world
+      </Button>
     </>
   );
 }
