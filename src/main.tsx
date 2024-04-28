4c95d5f5452530f16ecb839752f7ba0aad02eb2d
Zlaam <mrzalaam@gmail.com>
2024-04-28 18:42:27 +0500

    setup react router for the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import { router } from "@/router.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
