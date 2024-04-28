73fbf51dc285f71165a16bae03996a535c8df23b
Zlaam <mrzalaam@gmail.com>
2024-04-28 19:25:45 +0500

    move 2 files and update 1 file

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import LoginPage from "@/pages/auth/Login/LoginPage";
+import LoginPage from "@/pages/auth/LoginPage";
 import HomePage from "@/pages/Home/HomePage";
-import RegisterPage from "@/pages/auth/Login/RegisterPage";
+import RegisterPage from "@/pages/auth/RegisterPage";
 export const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/login", element: <LoginPage /> },
