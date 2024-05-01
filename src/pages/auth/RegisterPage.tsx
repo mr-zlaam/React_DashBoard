57b6706bc69e0051a23cfddec845b6ee25f1a1f6
Zlaam <mrzalaam@gmail.com>
2024-05-01 20:14:39 +0500

    update RegisterPage.tsx

--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -8,14 +8,39 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {} from "react";
-import { Link } from "react-router-dom";
+import { register } from "@/http/api";
+import { useMutation } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function RegisterPage() {
+  const navigate = useNavigate();
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+  const usernameRef = useRef<HTMLInputElement | null>(null);
+  const fullname = useRef<HTMLInputElement | null>(null);
+
+  const mutation = useMutation({
+    mutationFn: register,
+    onSuccess: () => {
+      console.log("Login Successfull");
+      navigate("/dashboard/home");
+    },
+  });
+  const handleRegisterSubmit = () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    const username = usernameRef.current?.value;
+    const displayName = fullname.current?.value;
+    if (!email || !password || !username || !displayName)
+      return alert("Please fill all the fields");
+    mutation.mutate({ email, password, username, displayName });
+  };
   return (
     <>
       <section className="flex items-center justify-center h-screen">
         <Card className="w-full max-w-sm">
+          {mutation.isError && <p>{mutation.error.message}</p>}
           <CardHeader>
             <CardTitle className="text-2xl text-center">Sign Up</CardTitle>
             <CardDescription className="text-center">
@@ -27,16 +52,27 @@ function RegisterPage() {
               <div className="grid gap-4">
                 <div className="grid gap-2">
                   <Label htmlFor="username">Username</Label>
-                  <Input id="username" placeholder="john_doe" required />
+                  <Input
+                    ref={usernameRef}
+                    id="username"
+                    placeholder="john_doe"
+                    required
+                  />
                 </div>
                 <div className="grid gap-2">
                   <Label htmlFor="fullname">Full Name</Label>
-                  <Input id="fullname" placeholder="John Doe" required />
+                  <Input
+                    ref={fullname}
+                    id="fullname"
+                    placeholder="John Doe"
+                    required
+                  />
                 </div>
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
                 <Input
+                  ref={emailRef}
                   id="email"
                   type="email"
                   placeholder="john@example.com"
@@ -45,9 +81,18 @@ function RegisterPage() {
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" placeholder="••••••••" />
+                <Input
+                  ref={passwordRef}
+                  id="password"
+                  type="password"
+                  placeholder="••••••••"
+                />
               </div>
-              <Button type="submit" className="w-full">
+              <Button
+                onClick={handleRegisterSubmit}
+                type="submit"
+                className="w-full"
+              >
                 Create an account
               </Button>
             </div>
